d15887159b373eb90d864701d704c8a540172a5d
vikito11111 <vikito11111@gmail.com>
2026-01-30 16:49:02 +0200

    Add Game model schema

---
+++ server/src/models/Game.js
@@ -0,0 +1,110 @@
+const mongoose = require('mongoose');
+
+const gameSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Game title is required'],
+        trim: true
+    },
+    slug: {
+        type: String,
+        unique: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    shortDescription: {
+        type: String,
+        trim: true, 
+    },
+    price: {
+        type: Number,
+        required: [true, 'Game price is required'],
+        min: [0, 'Price cannot be negative'],
+    },
+    discountPrice: {
+        type: Number,
+        min: [0, 'Discount price cannot be negative'],
+    },
+    images: {
+        cover: String,
+        banner: String,
+        screenshots: [String],
+    },
+    genre: [{
+        type: String,
+        trim: true,
+    }],
+    tags: [{
+        type: String,
+        trim: true,
+    }],
+    developer: {
+        type: String,
+        trim: true,
+    },
+    publisher: {
+        type: String,
+        trim: true,
+    },
+    releaseDate: {
+        type: Date,
+    },
+    platforms: [{
+        type: String,
+        trim: true,
+    }],
+    systemRequirements: {
+        minimum: {
+            os: String,
+            processor: String,
+            memory: String,
+            graphics: String,
+            storage: String,
+        },
+        recommended: {
+            os: String,
+            processor: String,
+            memory: String,
+            graphics: String,
+            storage: String,
+        },
+    },
+    ratings: {
+        average: {
+            type: Number,
+            default: 0,
+            min: 0,
+            max: 10,
+        },
+        count: {
+            type: Number,
+            default: 0,
+        }
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+}, {
+    timestamps: true,
+});
+
+gameSchema.pre('save', function(next) {
+    if (this.isModified('title')) {
+        this.slug = this.title
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/g, '-')
+            .replace(/(^-|-$)/g, '');
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('Game', gameSchema);
