7e94fe8b03e4359420de43715ce41a6dae55f5ea
vikito11111 <vikito11111@gmail.com>
2026-02-01 12:40:13 +0200

    Add model exports for User, Game, Library, Wishlist, Review, Order, Cart, Category, Thread, and Post

---
+++ server/src/models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Game = require('./Game');
+const Library = require('./Library');
+const Wishlist = require('./Wishlist');
+const Review = require('./Review');
+const Order = require('./Order');
+const Cart = require('./Cart');
+const Category = require('./Forum/Category');
+const Thread = require('./Forum/Thread');
+const Post = require('./Forum/Post');
+
+module.exports = {
+    User,
+    Game,
+    Library,
+    Wishlist,
+    Review,
+    Order,
+    Cart,
+    Category,
+    Thread,
+    Post
+};
